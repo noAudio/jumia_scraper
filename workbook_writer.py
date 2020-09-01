36237b6d6e8bde5ab0e8bb4a3ddb9e6ffde54951
Samuel Odongo <samieodd@gmail.com>
2020-09-01 18:42:26 +0300

    Make function close workbook outside of loop

--- workbook_writer.py
+++ workbook_writer.py
@@ -1,13 +1,16 @@
 import xlsxwriter
 
-def write_to_workbook(row, file, dictionary):
+def write_to_workbook(row, file, products):
     '''
         Takes the list of products and writes it to an Excel File.
     '''
     workbook = xlsxwriter.Workbook(file)
     worksheet = workbook.add_worksheet()
-    worksheet.write(f'A{row}', dictionary['title'])
-    worksheet.write(f'B{row}', dictionary['link'])
-    worksheet.write(f'C{row}', dictionary['price'])
-    
+
+    for dictionary in products:
+        worksheet.write(f'A{row}', dictionary['title'])
+        worksheet.write(f'B{row}', dictionary['link'])
+        worksheet.write(f'C{row}', dictionary['price'])
+        row += 1
+
     workbook.close()
